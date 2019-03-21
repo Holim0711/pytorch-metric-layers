718e484c09f8317d326fe2da0784021eb3706eca
Holim <ihl7029@europa.snu.ac.kr>
2019-03-21 16:02:59 +0900

    expanded interface

--- métrics/multiclass/basic.py
+++ métrics/multiclass/basic.py
@@ -1,34 +1,59 @@
 class AccTopK():
-    def __init__(self, k=1):
-        self.k = k
-        self.right = 0
+    def __init__(self, k=[1]):
+        self.k = sorted(k, reverse=True)
+        self.right = [0] * len(k)
         self.total = 0
 
     def update(self, pred, true):
-        topk = pred.argsort()[:, -self.k:]
-        self.right += (topk == true.view(-1, 1)).any(dim=1).sum().item()
+        rank = pred.argsort(descending=True)
+        for i, k in enumerate(self.k):
+            rank = rank[:, :k]
+            self.right[i] += (rank == true.view(-1, 1)).any(dim=1).sum().item()
         self.total += len(true)
 
     def commit(self):
-        acc = self.right / self.total
+        acc = {
+            ("accuracy@%d" % k) : (x / self.total)
+            for k, x in zip(self.k, self.right)
+        }
         self.__init__()
         return acc
 
 
 if __name__ == "__main__":
-    k = 3
-    topk = AccTopK(k)
 
     import torch
-    for _ in range(2):
-        pred = torch.randn(3, 8)
-        true = torch.randint(8, (3,))
-        print("pred:")
-        print(pred)
-        print("top-k pred:")
-        print(pred.argsort()[:, -k:])
-        print("true:")
-        print(true)
-        topk.update(pred, true)
+    from time import time
 
-    print(topk.commit())
+    pred = [
+        torch.tensor([
+            [ 0.43, -0.23,  1.16, -0.22,  1.08, -0.78,  0.31,  0.27],
+            [-0.55, -1.45,  0.96,  1.28, -0.27, -0.14, -0.38,  0.01],
+            [-1.07,  1.03, -0.04, -1.83,  0.33,  0.04,  1.78, -1.22]
+        ]),
+        torch.tensor([
+            [-1.15,  0.09,  1.03,  0.58, -1.18, -0.09, -1.60,  0.19],
+            [ 0.08,  0.54,  1.31,  1.39,  1.28, -0.96, -1.63, -0.37],
+            [-1.24,  1.79, -0.65, -0.28,  0.48,  0.38, -0.36,  1.82]
+        ]),
+    ]
+
+    true = [
+        torch.tensor([1, 7, 4]),
+        torch.tensor([6, 6, 3]),
+    ]
+
+
+    def Test_AccTopK():
+        topk = AccTopK([3, 5])
+
+        start = time()
+        for p, t in zip(pred, true):
+            topk.update(p, t)
+        print("Time LRAP:", time() - start)
+
+        result = topk.commit()
+        assert(result['accuracy@3'] == (2 / 6))
+        assert(result['accuracy@5'] == (3 / 6))
+
+    Test_AccTopK()
--- métrics/multilabel/basic.py
+++ métrics/multilabel/basic.py
@@ -2,24 +2,32 @@ from sklearn.metrics import label_ranking_average_precision_score as lrap_score
 
 
 class PrcRecTopK():
-    def __init__(self, k=1):
-        self.k = k
-        self.right = 0
+    def __init__(self, k=[1]):
+        self.k = sorted(k, reverse=True)
+        self.right = [0] * len(k)
         self.n_label = 0
         self.n_sample = 0
 
     def update(self, pred, true):
-        topk = pred.argsort()[:, -self.k:]
-        for p, q in zip(topk, true):
-            self.right += q[p].long().sum().item()
-            self.n_label += q.long().sum().item()
+        rank = pred.argsort(descending=True)
+        for i, k in enumerate(self.k):
+            rank = rank[:, :k]
+            for p, q in zip(rank, true):
+                self.right[i] += q[p].long().sum().item()
+        self.n_label += true.long().sum().item()
         self.n_sample += len(true)
 
     def commit(self):
-        precision = self.right / (self.k * self.n_sample)
-        recall = self.right / self.n_label
+        results = {
+            ("precision@%d" % k) : x / (k * self.n_sample)
+            for x, k in zip(self.right, self.k)
+        }
+        results.update({
+            ("recall@%d" % k) : x / self.n_label
+            for x, k in zip(self.right, self.k)
+        })
         self.__init__()
-        return precision, recall
+        return results
 
 
 class LRAP():
@@ -34,25 +42,66 @@ class LRAP():
     def commit(self):
         lrap = self.Σ / self.n_sample
         self.__init__()
-        return lrap
+        return {"lrap" : lrap}
 
 
 if __name__ == "__main__":
-    k = 3
-    topk = PrcRecTopK(k)
-    lrap = LRAP()
-
     import torch
     from time import time
 
-    start = time()
-    for _ in range(100):
-        pred = torch.randn(32, 1000)
-        true = torch.randint(2, (32, 1000))
-        topk.update(pred, true)
-        lrap.update(pred, true)
-    end = time()
+    pred = [
+        torch.tensor([
+            [-0.4, -0.2, +0.9, -0.5, +0.8, -0.9, -0.1, -1.5],
+            [+1.6, -0.5, +0.0, -0.5, +1.4, -0.8, +0.5, -2.2],
+            [-1.7, -2.7, +0.3, -0.1, -0.0, +1.1, +0.4, -1.1]
+        ]),
+        torch.tensor([
+            [+0.9, -0.4, -0.5, -0.1, +0.2, +0.4, +1.6, +1.0],
+            [+0.2, -0.7, +0.8, -0.8, -0.9, -0.7, -0.0, -0.1],
+            [-0.2, -0.4, +1.0, -0.2, -1.9, +2.2, -0.3, +0.4]
+        ]),
+    ]
+    true = [
+        torch.tensor([
+            [0, 1, 1, 1, 1, 0, 1, 1],
+            [1, 1, 0, 0, 1, 1, 1, 1],
+            [1, 0, 0, 1, 1, 0, 1, 1]
+        ]),
+        torch.tensor([
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 1, 0, 1, 1, 0, 0],
+            [0, 0, 0, 0, 1, 1, 0, 0]
+        ]),
+    ]
 
-    print(end - start)
-    print(topk.commit())
-    print(lrap.commit())
+    def Test_PrcRecTopK():
+        topk = PrcRecTopK([1, 3, 5])
+
+        start = time()
+        for p, t in zip(pred, true):
+            topk.update(p, t)
+        print("Time PrcRecTopK:", time() - start)
+
+        result = topk.commit()
+        assert(result['precision@1'] == (4 / 6))
+        assert(result['precision@3'] == (10 / 18))
+        assert(result['precision@5'] == (14 / 30))
+        assert(result['recall@1'] == (4 / 23))
+        assert(result['recall@3'] == (10 / 23))
+        assert(result['recall@5'] == (14 / 23))
+
+    def Test_LRAP():
+        lrap = LRAP()
+
+        start = time()
+        for p, t in zip(pred, true):
+            lrap.update(p, t)
+        print("Time LRAP:", time() - start)
+
+        result = lrap.commit()
+
+        gold = lrap_score(torch.cat(true), torch.cat(pred))
+        assert(result['lrap'] == gold)
+
+    Test_PrcRecTopK()
+    Test_LRAP()
