829541aa2b3d07e2fb72a717677ec73765041086
Holim Lim <holim0711@outlook.com>
2020-05-01 05:28:44 +0900

    fix names

--- métrics/loggers/multiclass_logger.py
+++ métrics/loggers/multiclass_logger.py
@@ -1,6 +1,6 @@
 from .logger import SimpleLogger
 from numpy import ndarray
-from torch import tensor
+from torch import Tensor
 from numbers import Number
 
 
@@ -9,14 +9,14 @@ class MulticlassLogger(SimpleLogger):
     def write(self, output, target):
         assert len(output) == len(target)
 
-        if isinstance(output, (ndarray, tensor)):
+        if isinstance(output, (ndarray, Tensor)):
             assert len(output.shape) == 2
             output = output.tolist()
         else:
             assert all(len(x) == len(output[0]) for x in output)
             assert all(isinstance(y, Number) for x in output for y in x)
 
-        if isinstance(target, (ndarray, tensor)):
+        if isinstance(target, (ndarray, Tensor)):
             assert len(target.shape) == 1
             target = target.tolist()
         else:
