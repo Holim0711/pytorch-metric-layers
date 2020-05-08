5c3700e5eca8d570df5b0f3c0a0a9d91ef423afc
Holim Lim <holim0711@outlook.com>
2020-05-09 02:03:20 +0900

    AP score

--- métrics/multilabel.py
+++ métrics/multilabel.py
@@ -1,14 +1,17 @@
 import json
 from statistics import mean
+from itertools import chain
 import numpy as np
 from sklearn.metrics import recall_score
+from sklearn.metrics import average_precision_score
 from sklearn.metrics import label_ranking_average_precision_score
 from .utils import read_logfile
 
 
 __all__ = [
     'multilabel_recall_score',
-    'lrap_score',
+    'multilabel_AP_score',
+    'lrAP_score',
 ]
 
 
@@ -19,7 +22,7 @@ def make_topk_multihot(pred, k):
     return pred.tolist()
 
 
-def multilabel_recall_score(filename, t=None, method='perclass'):
+def multilabel_recall_score(filename, t=None, method='macro'):
     data = read_logfile(filename)
 
     true, pred = [*zip(*data)]
@@ -31,17 +34,33 @@ def multilabel_recall_score(filename, t=None, method='perclass'):
     else:
         raise ValueError(f"Unknown type of t: {type(t)}")
 
-    if method == 'perclass':
-        true, pred = [*zip(*true)], [*zip(*pred)]
-        return mean([recall_score(y, ŷ) for y, ŷ in zip(true, pred)])
-    elif method == 'overall':
-        ΣNc = sum(x == (1, 1) for y, ŷ in zip(true, pred) for x in zip(y, ŷ))
-        ΣNt = sum(sum(x) for x in true)
-        return ΣNc / ΣNt
+    if method == 'macro':
+        true = [*zip(*true)]
+        pred = [*zip(*pred)]
+        return mean(recall_score(y, ŷ) for y, ŷ in zip(true, pred))
+    elif method == 'micro':
+        true = chain.from_iterable(true)
+        pred = chain.from_iterable(pred)
+        return recall_score(true, pred)
     else:
         raise ValueError(f"Unknown method: {method}")
 
 
-def lrap_score(filename):
+def multilabel_AP_score(filename, method='macro'):
+    data = read_logfile(filename)
+
+    true, pred = [*zip(*data)]
+
+    if method == 'macro':  # case of mAP
+        true = [*zip(*true)]
+        pred = [*zip(*pred)]
+        return mean(average_precision_score(y, ŷ) for y, ŷ in zip(true, pred))
+    elif method == 'micro':
+        return average_precision_score(true, pred, average='micro')
+    else:
+        raise ValueError(f"Unknown method: {method}")
+
+
+def lrAP_score(filename):
     data = read_logfile(filename)
     return label_ranking_average_precision_score(*zip(*data))
