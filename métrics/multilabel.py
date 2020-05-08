1222a954af9f85d968ded50b50bfd6139fd8033c
Holim Lim <holim0711@outlook.com>
2020-05-09 02:25:55 +0900

    add precision & f1 score

--- métrics/multilabel.py
+++ métrics/multilabel.py
@@ -2,14 +2,18 @@ import json
 from statistics import mean
 from itertools import chain
 import numpy as np
+from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
 from sklearn.metrics import average_precision_score
 from sklearn.metrics import label_ranking_average_precision_score
 from .utils import read_logfile
 
 
 __all__ = [
+    'multilabel_precision_score',
     'multilabel_recall_score',
+    'multilabel_f1_score',
     'multilabel_AP_score',
     'lrAP_score',
 ]
@@ -22,43 +26,61 @@ def make_topk_multihot(pred, k):
     return pred.tolist()
 
 
+def quantize(pred, t=None):
+    if t == 0 or isinstance(t, float):
+        return [[y > t for y in x] for x in pred]
+    elif isinstance(t, int):
+        return [make_topk_multihot(x, t) for x in pred]
+    else:
+        raise ValueError(f"Unknown type of t: {type(t)}")
+
+
+def calculate(func, true, pred, method):
+    if method == 'macro':
+        true = [*zip(*true)]
+        pred = [*zip(*pred)]
+        return mean(func(y, ŷ) for y, ŷ in zip(true, pred))
+    elif method == 'micro':
+        true = chain.from_iterable(true)
+        pred = chain.from_iterable(pred)
+        return func(true, pred)
+    else:
+        raise ValueError(f"Unknown method: {method}")
+
+
+def multilabel_precision_score(filename, t=None, method='macro'):
+    data = read_logfile(filename)
+
+    true, pred = [*zip(*data)]
+
+    pred = quantize(pred, t)
+
+    return calculate(precision_score, true, pred, method)
+
+
 def multilabel_recall_score(filename, t=None, method='macro'):
     data = read_logfile(filename)
 
     true, pred = [*zip(*data)]
 
-    if t == 0 or isinstance(t, float):
-        pred = [[y > t for y in x] for x in pred]
-    elif isinstance(t, int):
-        pred = [make_topk_multihot(x, t) for x in pred]
-    else:
-        raise ValueError(f"Unknown type of t: {type(t)}")
+    pred = quantize(pred, t)
 
-    if method == 'macro':
-        true = [*zip(*true)]
-        pred = [*zip(*pred)]
-        return mean(recall_score(y, ŷ) for y, ŷ in zip(true, pred))
-    elif method == 'micro':
-        true = chain.from_iterable(true)
-        pred = chain.from_iterable(pred)
-        return recall_score(true, pred)
-    else:
-        raise ValueError(f"Unknown method: {method}")
+    return calculate(recall_score, true, pred, method)
 
 
-def multilabel_AP_score(filename, method='macro'):
+def multilabel_f1_score(filename, t=None, method='macro'):
     data = read_logfile(filename)
 
     true, pred = [*zip(*data)]
 
-    if method == 'macro':  # case of mAP
-        true = [*zip(*true)]
-        pred = [*zip(*pred)]
-        return mean(average_precision_score(y, ŷ) for y, ŷ in zip(true, pred))
-    elif method == 'micro':
-        return average_precision_score(true, pred, average='micro')
-    else:
-        raise ValueError(f"Unknown method: {method}")
+    pred = quantize(pred, t)
+
+    return calculate(f1_score, true, pred, method)
+
+
+def multilabel_AP_score(filename, method='macro'):
+    data = read_logfile(filename)
+    return calculate(average_precision_score, *zip(*data), method)
 
 
 def lrAP_score(filename):
